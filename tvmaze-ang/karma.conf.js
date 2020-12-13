bf0e5b3e0cc41e281a242a12c4d94af767dd7817
csaba szabo <csabattilaszabo@gmail.com>
2020-12-13 20:36:00 +0100

    update kharma reporter

--- tvmaze-ang/karma.conf.js
+++ tvmaze-ang/karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'spec'],
+    reporters: ['spec', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
