be03f54ef2b9ca5aabbfbce7264e5127ba3ca62f
csaba szabo <csabattilaszabo@gmail.com>
2020-12-15 21:04:26 +0100

    add error handling to show request

--- tvmaze-ang/src/app/shared/show/show.component.ts
+++ tvmaze-ang/src/app/shared/show/show.component.ts
@@ -1,9 +1,9 @@
-import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
 import {ShowService} from './show.service';
 import {Show} from '../../types';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {Observable} from 'rxjs';
-import {map, take} from 'rxjs/operators';
+import {EMPTY, Observable} from 'rxjs';
+import {catchError, map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-show',
@@ -13,8 +13,10 @@ import {map, take} from 'rxjs/operators';
 })
 export class ShowComponent implements OnInit {
   show$: Observable<Show> | undefined;
+  loadingText = '...loading';
 
   constructor(
+    private readonly cd: ChangeDetectorRef,
     private readonly showService: ShowService,
     @Inject(MAT_DIALOG_DATA) public data: { id: string }
   ) {
@@ -23,7 +25,14 @@ export class ShowComponent implements OnInit {
   ngOnInit(): void {
     this.show$ = this.showService.show(this.data.id)
       .pipe(
-        take(1), // only interested in the first value
+        take(1),
+        // only interested in the first value
+        catchError((error) => {
+          this.loadingText = error;
+          this.cd.detectChanges();
+
+          return EMPTY;
+        }),
         map(({show}) => {
           return show;
         })
